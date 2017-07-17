fa1d64d4aa2a1e4ce302862c32a3c8377c40f8b8
Ben Wiley <bjwiley2@gmail.com>
2017-07-17 13:49:20 -0400

    Get data from API in instance

--- code.js
+++ code.js
@@ -1,15 +1,18 @@
 const userId = 1;
 const apiUrl = `https://todo.timleland.com/api/user/${userId}/task/`;
 
-ajax.get(apiUrl, (tasks) => {
-  console.log(tasks);
-});
-
 window.vm = new Vue({
-  el: "#app",
+  el: '#app',
   data: {
     tasks: [
-      { task: "Get these tasks from the API" }
+      {}
     ]
+  },
+  mounted () {
+    const self = this;
+
+    ajax.get(apiUrl, (tasks) => {
+      self.tasks = tasks;
+    });
   }
 });
