0830933e82d1bc869ceeb86cd195d6ad0fd9f2b4
Ben Wiley <bjwiley2@gmail.com>
2017-07-31 10:46:04 -0400

    Add form component

--- code.js
+++ code.js
@@ -1,15 +1,47 @@
 const userId = 1;
 const apiUrl = `https://todo.timleland.com/api/user/${userId}/task/`;
 
+var taskFormComponent = {
+  props: {
+    task: {
+      type: Object,
+      required: true
+    },
+  },
+  methods: {
+    saveTask: function () {
+      var self = this;
+      self.$emit('cancel');
+      ajax.put(apiUrl + self.task.id, self.task);
+    },
+    cancelEdit: function () {
+      this.$emit('cancel');
+    }
+  },
+  template: '#task-form-template'
+};
+
 window.vm = new Vue({
   el: '#app',
-  data: {
-    tasks: []
+  components: {
+    taskForm: taskFormComponent
   },
-  mounted () {
-    const self = this;
+  data: {
+    tasks: [],
+    editingTask: null
+  },
+  methods: {
+    editTask: function (task) {
+      this.editingTask = task;
+    },
+    cancelEdit: function () {
+      this.editingTask = null;
+    }
+  },
+  mounted: function () {
+    var self = this;
 
-    ajax.get(apiUrl, (tasks) => {
+    ajax.get(apiUrl, function (tasks) {
       self.tasks = tasks;
     });
   }
