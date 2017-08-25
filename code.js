50a1931bf2fb5be4290d52913ac570f61139508f
Ben Wiley <bjwiley2@gmail.com>
2017-08-25 11:11:28 -0400

    Wrap date input

--- code.js
+++ code.js
@@ -1,16 +1,41 @@
-Vue.component('hoverCard', {
-    mounted: function () {
-        $(this.$el).hoverCard();
+Vue.component('datePicker', {
+    props: {
+      value: [String, Date]
     },
-    template:
-      '<div class="hover-card">' +
-        '<a tabindex="-1" href="javascript:void(0);">' +
-          '<i class="fa fa-info-circle" aria-hidden="true"></i>' +
-        '</a>' +
-        '<div class="hover-detail slide-right bottom-positioned">' +
-          '<slot></slot>' +
-        '</div>' +
-      '</div>'
+    methods: {
+      formatValueForPlugin: function (date) {
+        if (date === null) {
+          return null;
+        }
+
+        date = new Date(date);
+        return date.toLocaleDateString();
+      },
+      handleChange: function (value) {
+        var self = this;
+        self.$emit('input', value);
+        self.$emit('change', value);
+      },
+      setValue: function () {
+        var self = this;
+        var formattedValue = self.formatValueForPlugin(this.value);
+        $(self.$el).datePicker('setValue', formattedValue);
+      }
+    },
+    watch: {
+      value: function () {
+        var self = this;
+        self.setValue();
+      }
+    },
+    mounted: function () {
+      var self = this;
+      var el = $(self.$el);
+      el.data('changeCallback', self.handleChange);
+      el.datePicker();
+      self.setValue();
+    },
+    template: '<input type="text" />'
 });
 
 Vue.directive('focus', {
@@ -38,7 +63,8 @@ var taskFormComponent = {
     var self = this;
 
     return {
-      taskText: self.task ? self.task.task : ''
+      taskText: self.task ? self.task.task : '',
+      dateAdded: self.task ? self.task.dateAdded : new Date()
     };
   },
   computed: {
@@ -54,10 +80,15 @@ var taskFormComponent = {
   methods: {
     handleSubmit: function () {
       var self = this;
-      self.$emit('submit', self.taskText);
+      self.$emit('submit', {
+        task: self.taskText,
+        dateAdded: self.dateAdded,
+        completed: self.task ? self.task.completed : false
+      });
 
       if (self.isAdd) {
         self.taskText = '';
+        self.dateAdded = new Date();
       }
     }
   },
@@ -84,18 +115,12 @@ window.vm = new Vue({
       });
     },
     methods: {
-      addNewTask: function (text) {
+      addNewTask: function (task) {
         var self = this;
 
-        var newTask = {
-          completed: false,
-          dateAdded: new Date(),
-          task: text
-        };
-
-        api.create(newTask, function (newId) {
-          newTask.id = newId;
-          self.tasks.push(newTask);
+        api.create(task, function (newId) {
+          task.id = newId;
+          self.tasks.push(task);
           self.newTaskText = '';
         });
       },
@@ -111,9 +136,10 @@ window.vm = new Vue({
         self.editingTask = task;
         self.editTaskText = task.task;
       },
-      editTask: function (text) {
+      editTask: function (task) {
         var self = this;
-        self.editingTask.task = text;
+        self.editingTask.task = task.task;
+        self.editingTask.dateAdded = task.dateAdded;
 
         api.update(self.editingTask, function () {
           self.editingTask = null;
