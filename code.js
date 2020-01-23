e8f1cd08c73783fff1cfc38eab14645fec9b9df8
Olga-Pakhotin <56894101+Olga-Pakhotin@users.noreply.github.com>
2020-01-23 12:15:13 -0500

    add delet button

--- code.js
+++ code.js
@@ -1,5 +1,8 @@
+// Register a global custom directive called `v-focus`
 Vue.directive('focus', {
+    // When the bound element is inserted into the DOM...
     inserted: function (el) {
+        // Focus the element
         el.focus()
     }
 })
@@ -7,7 +10,7 @@ Vue.directive('focus', {
 
 window.vm = new Vue({
     el: '#app',
-    data: function () {
+    data: function() {
         return {
             heading: 'To Do List',
             tasks: [],
@@ -15,30 +18,44 @@ window.vm = new Vue({
         };
     },
     //lifecycle hook to call a method to get a list of tasks
-    created: function () {
+    created: function() {
         this.getTasks();
     },
     methods: {
         // method to get a list of tasks
-        getTasks: function () {
+        getTasks: function() {
             var self = this;
-            api.getList(function (tasks) {
+            api.getList(function(tasks) {
                 self.tasks = tasks;
             });
         },
-       // method to add a Task to the list
-        addTask: function () {
+        // method to add a Task to the list
+        addTask: function() {
             var self = this;
             var task = {
                 task: self.newTask
             };
             //create a task, and clear the input
-            api.create(task, function (id) {
-                api.get(id, function (task) {
-                    self.tasks.push(task);
-                    self.newTask = null;
+            api.create(task,
+                function(id) {
+                    api.get(id,
+                        function(task) {
+                            self.tasks.push(task);
+                            self.newTask = null;
+                        });
                 });
-            });
+        },
+        // method to delete a Task from the list
+         deleteTask: function() {
+            var self = this;
+            var task = this.task;
+            api.delete(task,
+                function (id) {
+                    api.delete(id,
+                        function (task) {
+                            delete(tasks.task);
+                        });
+                });
         }
     }
 });
