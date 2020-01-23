1fd885f820acac5ca027b5dd05054d95ade0e5bc
Olga-Pakhotin <56894101+Olga-Pakhotin@users.noreply.github.com>
2020-01-23 10:34:39 -0500

    add comments

--- code.js
+++ code.js
@@ -14,22 +14,25 @@ window.vm = new Vue({
             newTask: null
         };
     },
+    //lifecycle hook to call a method to get a list of tasks
     created: function () {
         this.getTasks();
     },
     methods: {
+        // method to get a list of tasks
         getTasks: function () {
             var self = this;
             api.getList(function (tasks) {
                 self.tasks = tasks;
             });
         },
+       // method to add a Task to the list
         addTask: function () {
             var self = this;
             var task = {
                 task: self.newTask
             };
-
+            //create a task, and clear the input
             api.create(task, function (id) {
                 api.get(id, function (task) {
                     self.tasks.push(task);
