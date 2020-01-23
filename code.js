e4b92ecb716390e8c8394ceea3336751786dc44a
Olga-Pakhotin <56894101+Olga-Pakhotin@users.noreply.github.com>
2020-01-23 09:47:08 -0500

    Add newTask button

--- code.js
+++ code.js
@@ -1,13 +1,41 @@
-window.vm = new Vue({
-  el: '#app',
-  data: {
-    tasks: []
-  },
-  mounted () {
-    const self = this;
+Vue.directive('focus', {
+    inserted: function (el) {
+        el.focus()
+    }
+})
 
-    api.getList((tasks) => {
-      self.tasks = tasks;
-    });
-  }
+
+window.vm = new Vue({
+    el: '#app',
+    data: function () {
+        return {
+            heading: 'To Do List',
+            tasks: [],
+            newTask: null
+        };
+    },
+    created: function () {
+        this.getTasks();
+    },
+    methods: {
+        getTasks: function () {
+            var self = this;
+            api.getList(function (tasks) {
+                self.tasks = tasks;
+            });
+        },
+        addTask: function () {
+            var self = this;
+            var task = {
+                task: self.newTask
+            };
+
+            api.create(task, function (id) {
+                api.get(id, function (task) {
+                    self.tasks.push(task);
+                    self.newTask = null;
+                });
+            });
+        }
+    }
 });
