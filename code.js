b6af88eb5c0b0f115f25894ea4ae5786abaca14c
Olga-Pakhotin <56894101+Olga-Pakhotin@users.noreply.github.com>
2020-01-29 16:00:03 -0500

    add datepicker

--- code.js
+++ code.js
@@ -1,3 +1,43 @@
+Vue.component('datePicker', {
+    props: {
+        value: [String, Date]
+    },
+    methods: {
+        formatValueForPlugin: function (date) {
+            if (date === null) {
+                return null;
+            }
+
+            date = new Date(date);
+            return date.toLocaleDateString();
+        },
+        handleChange: function (value) {
+            var self = this;
+            self.$emit('input', value);
+            self.$emit('change', value);
+        },
+        setValue: function () {
+            var self = this;
+            var formattedValue = self.formatValueForPlugin(this.value);
+            $(self.$el).datePicker('setValue', formattedValue);
+        }
+    },
+    watch: {
+        value: function () {
+            var self = this;
+            self.setValue();
+        }
+    },
+    mounted: function () {
+        var self = this;
+        var el = $(self.$el);
+        el.data('changeCallback', self.handleChange);
+        el.datePicker();
+        self.setValue();
+    },
+    template: '<input type="text" />'
+});
+
 //Register a global custom directive called `v-focus`
 Vue.directive('focus', {
     // When the bound element is inserted into the DOM...
@@ -26,7 +66,8 @@ var taskFormComponent = {
         var self = this;
 
         return {
-            taskText: self.task ? self.task.task : ''
+            taskText: self.task ? self.task.task : '',
+            dateDue: self.task ? self.task.dateDue : new Date()
         };
     },
     computed: {
@@ -42,8 +83,11 @@ var taskFormComponent = {
     methods: {
         handleSubmit: function () {
             var self = this;
-            self.$emit('submit', self.taskText);
 
+            self.$emit('submit', {
+                task: self.taskText,
+                dateDue: self.dateDue
+            });
             if (self.isAdd) {
                 self.taskText = '';
             }
@@ -74,13 +118,14 @@ window.vm = new Vue({
     },
     methods: {
         // method to add a Task to the list
-        addNewTask: function (text) {
+        addNewTask: function (formData) {
             var self = this;
 
             var newTask = {
                 completed: false,
                 dateAdded: new Date(),
-                task: text
+                task: formData.task,
+                dateDue: formData.dateDue
             };
 
             api.create(newTask, function (newId) {
@@ -102,9 +147,11 @@ window.vm = new Vue({
             self.editingTask = task;
             self.editTaskText = task.task;
         },
-        editTask: function (text) {
+        editTask: function (formData) {
             var self = this;
-            self.editingTask.task = text;
+
+            self.editingTask.task = formData.task;
+            self.editingTask.dateDue = formData.dateDue;
 
             api.update(self.editingTask, function () {
                 self.editingTask = null;
